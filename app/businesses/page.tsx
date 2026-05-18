6ca52387c835bcb91580e8580b7da976fada63b8
SMissing <sammissing98@gmail.com>
2026-05-18 15:42:49 +0100

    Change matt back

--- app/businesses/page.tsx
+++ app/businesses/page.tsx
@@ -18,7 +18,7 @@ export default async function BusinessesPage() {
       title: 'Site Management',
       roles: [
         { title: 'Garrison General Manager', people: ['James Harvey'] },
-        { title: 'Bassment Assistant Manager', people: ['Matt Douglas'] },
+        { title: 'Garrison Assistant Manager', people: ['Matt Douglas'] },
         { title: 'Spirits General Manager', people: ['TBN'] },
         { title: 'Bassment General Manager', people: ['Daisy King'] },
       ],
